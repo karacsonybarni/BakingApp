ccf5208c19aec892a5163fbc7619c68013ab12a2
barnakaracsony <git8.XyZ>
2019-06-28 10:51:54 +0200

    initial commit; recipe parsing tested

---
+++ app/src/main/java/com/udacity/bakingapp/data/database/entity/Recipe.java
@@ -0,0 +1,14 @@
+package com.udacity.bakingapp.data.database.entity;
+
+public class Recipe {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ app/src/main/java/com/udacity/bakingapp/util/json/Parser.java
@@ -0,0 +1,31 @@
+package com.udacity.bakingapp.util.json;
+
+import com.udacity.bakingapp.data.database.entity.Recipe;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parser {
+
+    private static final String NAME = "name";
+
+    public static List<Recipe> parseRecipes(String jsonString) throws JSONException {
+        JSONArray recipesJSONArray = new JSONArray(jsonString);
+        List<Recipe> recipes = new ArrayList<>();
+        for (int i = 0; i < recipesJSONArray.length(); i++) {
+            JSONObject recipeJSONObject = recipesJSONArray.getJSONObject(i);
+            recipes.add(parseRecipe(recipeJSONObject));
+        }
+        return recipes;
+    }
+
+    private static Recipe parseRecipe(JSONObject jsonObject) throws JSONException {
+        Recipe recipe = new Recipe();
+        recipe.setName(jsonObject.getString(NAME));
+        return recipe;
+    }
+}
